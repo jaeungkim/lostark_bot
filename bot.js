f116293c1366cbf0601d717d237ad349ce4f9443
Jae Kim <43080200+jaeungkim@users.noreply.github.com>
2022-04-26 15:48:34 -0700

    ccc

--- bot.js
+++ bot.js
@@ -12,12 +12,12 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 })
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-
-    if (interaction.commandName === '기모띠') {
-        await interaction.reply('앙기모띠!');
+client.on('messageCreate', (message) => {
+    if (message.content === '기모띠') {
+        message.reply({
+            content: '앙d',
+        })
     }
-});
+})
 
 client.login(process.env.BOT_TOKEN)
