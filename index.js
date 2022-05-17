2b355ba0176332ed9d9812f62dd96ff609750f94
Jae Kim <43080200+jaeungkim@users.noreply.github.com>
2022-05-17 14:54:33 -0700

    220517 V.1.7.0

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ client.on('interactionCreate', async interaction => {
     const command = client.commands.get(interaction.commandName);
     try {
         if (interaction.commandName === 'party') {
-            // interaction.reply({ content: `파티 모집`, ephemeral: true })
+            interaction.reply({ content: `파티 모집`, ephemeral: true })
             command.execute(interaction, client)
         }
     } catch (error) {
