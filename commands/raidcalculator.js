6dc5f316cbaf024c898c30c4195d7b184f93f12e
Jae Kim <43080200+jaeungkim@users.noreply.github.com>
2022-06-26 15:24:49 -0700

    update

--- commands/raidcalculator.js
+++ commands/raidcalculator.js
@@ -26,10 +26,10 @@ module.exports = {
         const partysize = interaction.options.get('partysize').value; //인원
         const profitprice = Math.round(price * 0.95 * (partysize - 1) / partysize); //손익분기점 (길드팟 분배 손익분기점)
         const eachpartyprice = Math.round(price * 0.95 * (1 / partysize)); //분배금 (개인 분배금)
-        const sellprice = price - profitprice; // 판매차익
+        const sellprice = (price * 0.95) - profitprice; // 판매차익
         const selfsellprice = Math.ceil(profitprice / 1.1); //입찰적정가 (본인이 사서 팔때 최대 추천 입찰 가격)
         const distributedprice = Math.round(selfsellprice / (partysize - 1 / partysize)); //개인 최대 입찰시 안산사람 인당 분배금
-        const individualsellpriceprofit = price - selfsellprice;
+        const individualsellpriceprofit = (price * 0.95) - selfsellprice;
         const goldemoji = " <:GOLD:990737850767118406>";
         //embed
         const title = `템 가격: ${price}${goldemoji} (수수료 5%: ${price * 0.05}${goldemoji})`;
@@ -47,12 +47,12 @@ module.exports = {
                 { name: '파티 기준 계산법 입니다.', value: '(공대 / 지인)'},
                 { name: `손익분기점`, value: `${profitprice + goldemoji}`, inline: true},
                 { name: `분배금`, value: `${eachpartyprice + goldemoji}`, inline: true},
-                { name: '판매차익 (수수료 가격 포함 입니다.)', value: `${sellprice + goldemoji}`, inline: true},
+                { name: '판매차익', value: `${sellprice + goldemoji}`, inline: true},
                 { name: '\u200B', value: '\u200B' },
                 { name: '공팟 기준 계산법 입니다.', value: '(개인 이득)'},
                 { name: '입찰적정가', value: `${selfsellprice + goldemoji}`, inline: true},
                 { name: '분배금', value: `${distributedprice + goldemoji}`, inline: true},
-                { name: '판매차익 (수수료 가격 포함 입니다.)', value: `${individualsellpriceprofit + goldemoji}`, inline: true},
+                { name: '판매차익', value: `${individualsellpriceprofit + goldemoji}`, inline: true},
             )
             .setThumbnail('https://i.imgur.com/cE7xFGE.png')
             .setTimestamp()
