2051f56b7417eb2a755b3eaf4157838a0859c5c1
Jaeung Kim <43080200+jaeungkim@users.noreply.github.com>
2022-12-14 08:54:23 -0800

    Update index.js

--- index.js
+++ index.js
@@ -51,40 +51,6 @@ client.once('disconnect', () => {
     console.log('Disconnect!');
 });
 
-client.on('messageCreate', async message => {
-    if (message.author.bot || !message.guild) return;
-    if (!client.application?.owner) await client.application?.fetch();
-
-    const messsage = message.content.trim(); // message = !공략 발탄
-    let messageArray = messsage.split(" ") //[!공략, 발탄]
-    let prefix = client.prefixes.get(messageArray[0]);
-    if (prefix !== undefined && messageArray[0] === "!공략") { 
-        if (messageArray.length === 1) {
-            message.reply("공략은 발탄, 비아키스만 제공 합니다.");
-            return;
-        }
-        let raidName = messageArray[1]; //발탄, 비아키스
-        let guide = prefix.raids[raidName]; 
-        if (guide !== undefined) {
-            const exampleEmbed = new MessageEmbed().setColor('#0099ff').setImage(guide.thumbnail)
-                .setTitle(`${guide.title}`).setDescription(`${guide.description}`);
-            message.channel.send({ embeds: [exampleEmbed] })
-        }
-    }
-
-    // if (message.content === '!deploy' && message.author.id === client.application?.owner?.id) {
-    //     await message.guild.commands
-    //         .set(client.commands)
-    //         .then(() => {
-    //             message.reply('Deployed!');
-    //         })
-    //         .catch(err => {
-    //             message.reply('Could not deploy commands! Make sure the bot has the application.commands permission!');
-    //             console.error(err);
-    //         });
-    // }
-});
-
 // console.log(client.commands);
 
 client.on('interactionCreate', async interaction => {
