ff69cb6a2f87fdc43175daac6b138fce23e8157f
Jae Kim <43080200+jaeungkim@users.noreply.github.com>
2023-04-10 15:52:59 -0700

    230410 03:52pm

--- commands/party.js
+++ commands/party.js
@@ -177,7 +177,7 @@ module.exports = {
       "1069160814294085662": "https://i.imgur.com/YthsNCa.png", //아브14
       "1069160878798295090": "https://i.imgur.com/YthsNCa.png", //아브16
       "1095112450594050228": "https://i.imgur.com/ApXMhy6.png", //하브12
-      "1095112419572994058": "https://i.imgur.com/HbhOVFN.jpg", //하브14
+      "1095112419572994058": "https://i.imgur.com/BLZWYRE.jpg", //하브14
       "1095112648984625283": "https://i.imgur.com/h54Mkvz.jpg", //하브16
     };
     let limit = lineUp[partyName];
