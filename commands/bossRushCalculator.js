d1a8b3deda0539edaa11dbba25fadc022627b1fe
Jae Kim <43080200+jaeungkim@users.noreply.github.com>
2023-04-10 17:03:38 -0700

    230410 05:03pm

---
+++ commands/bossRushCalculator.js
@@ -0,0 +1,66 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    name: "bossRushCalculator",
+    description: "보스러쉬 보석 갯수 calculator",
+    options: [
+      {
+        name: "회랑 노말",
+        type: 3,
+        description: "회랑 노말 티켓 갯수"
+      },
+      {
+        name: "회랑 하드",
+        type: 3,
+        description: "회랑 하드 티켓 갯수"
+      },
+      {
+        name: "회랑 헬",
+        type: 3,
+        description: "회랑 헬 티켓 갯수"
+      },
+    ],
+    execute(interaction, client) {
+      const channel = client.channels.cache.get(interaction.channelId);
+      const normalTicket = interaction.options.get("회랑 노말").value;
+      const hardTicket = interaction.options.get("회랑 하드").value;
+      const hellTicket = interaction.options.get("회랑 헬").value;
+  
+      const title = `보스러쉬 티켓: ${typeofTicket}, 갯수: ${amountofTickets})`;
+      const description = `보스러쉬`;
+  
+      const fields = [
+        { name: "\u200B", value: "\u200B" },
+        {
+            name: "노말",
+            value: `${normalTicket}`,
+            inline: true,
+          },
+          {
+            name: "하드",
+            value: `${hardTicket}`,
+            inline: true,
+          },
+          {
+            name: "헬",
+            value: `${hellTicket}`,
+            inline: true,
+          },
+        { name: "\u200B", value: "\u200B" },
+      ]
+      const embed = new EmbedBuilder()
+        .setAuthor({
+          name: interaction.member.user.username,
+          iconURL: interaction.member.displayAvatarURL(),
+        })
+        .setColor("#0099ff")
+        .setTitle(title)
+        .setDescription(description)
+        .addFields(fields)
+        .setThumbnail("https://i.imgur.com/cE7xFGE.png")
+        .setTimestamp();
+  
+      channel.send({ content: "보스러쉬 계산기 입니다.", embeds: [embed] });
+    },
+  };
+  
