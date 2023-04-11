7358146c70ec1992593992beeb0d42e0985391e3
Jae Kim <43080200+jaeungkim@users.noreply.github.com>
2023-04-10 18:23:18 -0700

    230410

--- commands/bossrush.js
+++ commands/bossrush.js
@@ -21,19 +21,11 @@ function calculateRewards(normalTickets, hardTickets, hellTickets) {
   };
 
   const totalRewards = {
-    gems: normalRewards.gems.map(
-      (gem, index) =>
-        gem * normalTickets +
-        hardRewards.gems[index] * hardTickets +
-        hellRewards.gems[index] * hellTickets
-    ),
+    gems: normalRewards.gems.map((gem, index) => gem * normalTickets + hardRewards.gems[index] * hardTickets + hellRewards.gems[index] * hellTickets),
     honorLeapStones: normalTickets * normalRewards.honorLeapStones,
     greatHonorLeapStones: normalTickets * normalRewards.greatHonorLeapStones,
-    marvelousHonorLeapStones:
-      hardTickets * hardRewards.marvelousHonorLeapStones +
-      hellTickets * hellRewards.marvelousHonorLeapStones,
-    cardExp:
-      hardTickets * hardRewards.cardExp + hellTickets * hellRewards.cardExp,
+    marvelousHonorLeapStones: hardTickets * hardRewards.marvelousHonorLeapStones + hellTickets * hellRewards.marvelousHonorLeapStones,
+    cardExp: hardTickets * hardRewards.cardExp + hellTickets * hellRewards.cardExp,
   };
 
   return totalRewards;
@@ -70,53 +62,50 @@ module.exports = {
     },
   ],
 
+  
   execute(interaction, client) {
     const channel = client.channels.cache.get(interaction.channelId);
     const amountOfNormalTickets = interaction.options.get("normal")?.value ?? 0;
     const amountofHardTickets = interaction.options.get("hard")?.value ?? 0;
     const amountofHellTickets = interaction.options.get("hell")?.value ?? 0;
 
-    const totalRewards = calculateRewards(
-      amountOfNormalTickets,
-      amountofHardTickets,
-      amountofHellTickets
-    );
-    const combinedGems = combineGems(totalRewards.gems);
+    // const totalRewards = calculateRewards(amountOfNormalTickets, amountofHardTickets, amountofHellTickets);
+    // const combinedGems = combineGems(totalRewards.gems);
+    
+    const numberofLevel2Gems = 0;
+    const numberofLevel3Gems = 0;
+    const numberofLevel4Gems = 0;
+    const numberofLevel5Gems = 0;
+    const numberofLevel6Gems = 0;
+    const numberofLevel7Gems = 0;
+    const numberofLevel8Gems = 0;
+    const numberofLevel9Gems = 0;
+    const numberofLevel10Gems = 0;
+
+    const numberofHonorLeapStone = 0;
+    const numberofGreatHonorLeapStone = 0;
+    const numberofMarvelousHonorLeapStone = 0;
 
     const title = `티켓 갯수: 노말: ${amountOfNormalTickets} |  하드: ${amountofHardTickets} | 헬: ${amountofHellTickets}`;
     const description = `보스러쉬 계산기 입니다.`;
     const fields = [
       { name: "\u200B", value: "\u200B" },
-      { name: "2레벨", value: 0, inline: true },
-      { name: "3레벨", value: 0, inline: true },
-      { name: "4레벨", value: 0, inline: true },
-      { name: "5레벨", value: 0, inline: true },
-      { name: "6레벨", value: 0, inline: true },
-      { name: "7레벨", value: 0, inline: true },
-      { name: "8레벨", value: 0, inline: true },
-      { name: "9레벨", value: 0, inline: true },
-      { name: "10레벨", value: 0, inline: true },
+      { name: "2레벨", value: numberofLevel2Gems, inline: true },
+      { name: "3레벨", value: numberofLevel3Gems, inline: true },
+      { name: "4레벨", value: numberofLevel4Gems, inline: true },
+      { name: "5레벨", value: numberofLevel5Gems, inline: true },
+      { name: "6레벨", value: numberofLevel6Gems, inline: true },
+      { name: "7레벨", value: numberofLevel7Gems, inline: true },
+      { name: "8레벨", value: numberofLevel8Gems, inline: true },
+      { name: "9레벨", value: numberofLevel9Gems, inline: true },
+      { name: "10레벨", value: numberofLevel10Gems, inline: true },
       { name: "\u200B", value: "\u200B" },
-      { name: `명돌`, value: 0, inline: true },
-      { name: `위명돌`, value: 0, inline: true },
-      { name: "경명돌", value: 0, inline: true },
-      { name: "카드경험치", value: 0, inline: true },
+      { name: `명돌`, value: numberofHonorLeapStone, inline: true },
+      { name: `위명돌`, value: numberofGreatHonorLeapStone, inline: true },
+      { name: "경명돌", value: numberofMarvelousHonorLeapStone, inline: true },
+      { name: "카드경험치", value: "\u200B" },
       { name: "\u200B", value: "\u200B" },
     ];
-
-    fields.forEach((field, index) => {
-      if (index >= 1 && index <= 9) {
-        field.value = combinedGems[index - 1];
-      } else if (index === 11) {
-        field.value = totalRewards.honorLeapStones;
-      } else if (index === 12) {
-        field.value = totalRewards.greatHonorLeapStones;
-      } else if (index === 13) {
-        field.value = totalRewards.marvelousHonorLeapStones;
-      } else if (index === 14) {
-        field.value = totalRewards.cardExp;
-      }
-    });
 
     const embed = new EmbedBuilder()
       .setAuthor({
@@ -128,7 +117,7 @@ module.exports = {
       .setDescription(description)
       .addFields(fields)
       .setThumbnail("https://i.imgur.com/cE7xFGE.png")
-      .setFooter("\u3000".repeat(10) + "|")
+      .setFooter("\u3000".repeat(10)+"|")
       .setTimestamp();
 
     channel.send({ content: "보스러쉬 계산기 입니다.", embeds: [embed] });
