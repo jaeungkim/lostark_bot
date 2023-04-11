a82042f836cda3d2c44bf08f1a4ccfd9f2d7591c
Jae Kim <43080200+jaeungkim@users.noreply.github.com>
2023-04-10 18:17:21 -0700

    230410 06:17pm

--- commands/bossrush.js
+++ commands/bossrush.js
@@ -1,5 +1,54 @@
 const { EmbedBuilder } = require("discord.js");
 
+function calculateRewards(normalTickets, hardTickets, hellTickets) {
+  const normalRewards = {
+    gems: [0, 0, 1, 2, 0, 0, 0, 0, 0],
+    honorLeapStones: 17,
+    greatHonorLeapStones: 19,
+    cardExp: 0,
+  };
+
+  const hardRewards = {
+    gems: [0, 0, 0, 1, 2, 0, 0, 0, 0],
+    marvelousHonorLeapStones: 15,
+    cardExp: 8500,
+  };
+
+  const hellRewards = {
+    gems: [0, 2, 2, 2, 0, 0, 0, 0, 0],
+    marvelousHonorLeapStones: 26,
+    cardExp: 8500,
+  };
+
+  const totalRewards = {
+    gems: normalRewards.gems.map(
+      (gem, index) =>
+        gem * normalTickets +
+        hardRewards.gems[index] * hardTickets +
+        hellRewards.gems[index] * hellTickets
+    ),
+    honorLeapStones: normalTickets * normalRewards.honorLeapStones,
+    greatHonorLeapStones: normalTickets * normalRewards.greatHonorLeapStones,
+    marvelousHonorLeapStones:
+      hardTickets * hardRewards.marvelousHonorLeapStones +
+      hellTickets * hellRewards.marvelousHonorLeapStones,
+    cardExp:
+      hardTickets * hardRewards.cardExp + hellTickets * hellRewards.cardExp,
+  };
+
+  return totalRewards;
+}
+
+function combineGems(gems) {
+  for (let i = 0; i < gems.length - 1; i++) {
+    while (gems[i] >= 3) {
+      gems[i] -= 3;
+      gems[i + 1]++;
+    }
+  }
+  return gems;
+}
+
 module.exports = {
   name: "bossrush",
   description: "보스러쉬 calculator",
@@ -27,26 +76,42 @@ module.exports = {
     const amountofHardTickets = interaction.options.get("hard")?.value ?? 0;
     const amountofHellTickets = interaction.options.get("hell")?.value ?? 0;
 
+    const totalRewards = calculateRewards(
+      amountOfNormalTickets,
+      amountofHardTickets,
+      amountofHellTickets
+    );
+    const combinedGems = combineGems(totalRewards.gems);
+
     const title = `티켓 갯수: 노말: ${amountOfNormalTickets} |  하드: ${amountofHardTickets} | 헬: ${amountofHellTickets}`;
     const description = `보스러쉬 계산기 입니다.`;
     const fields = [
       { name: "\u200B", value: "\u200B" },
-      { name: "2레벨", value: "1", inline: true },
-      { name: "3레벨", value: "1", inline: true },
-      { name: "4레벨", value: "1", inline: true },
-      { name: "5레벨", value: "1", inline: true },
-      { name: "6레벨", value: "1", inline: true },
-      { name: "7레벨", value: "1", inline: true },
-      { name: "8레벨", value: "1", inline: true },
-      { name: "9레벨", value: "1", inline: true },
-      { name: "10레벨", value: "1", inline: true },
+      { name: "2레벨", value: 0, inline: true },
+      { name: "3레벨", value: 0, inline: true },
+      { name: "4레벨", value: 0, inline: true },
+      { name: "5레벨", value: 0, inline: true },
+      { name: "6레벨", value: 0, inline: true },
+      { name: "7레벨", value: 0, inline: true },
+      { name: "8레벨", value: 0, inline: true },
+      { name: "9레벨", value: 0, inline: true },
+      { name: "10레벨", value: 0, inline: true },
       { name: "\u200B", value: "\u200B" },
-      { name: `명돌`, value: `1`, inline: true },
-      { name: `위명돌`, value: `1`, inline: true },
-      { name: "경명돌", value: `1`, inline: true },
-      { name: "카드경험치", value: "\u200B" },
+      { name: `명돌`, value: 0, inline: true },
+      { name: `위명돌`, value: 0, inline: true },
+      { name: "경명돌", value: 0, inline: true },
+      { name: "카드경험치", value: 0, inline: true },
       { name: "\u200B", value: "\u200B" },
     ];
+
+    for (let i = 0; i < 9; i++) {
+      fields[i].value = combinedGems[i];
+    }
+    fields[11].value = totalRewards.honorLeapStones;
+    fields[12].value = totalRewards.greatHonorLeapStones;
+    fields[13].value = totalRewards.marvelousHonorLeapStones;
+    fields[14].name = "카드경험치";
+    fields[14].value = totalRewards.cardExp;
 
     const embed = new EmbedBuilder()
       .setAuthor({
@@ -58,7 +123,7 @@ module.exports = {
       .setDescription(description)
       .addFields(fields)
       .setThumbnail("https://i.imgur.com/cE7xFGE.png")
-      .setFooter("\u3000".repeat(10)+"|")
+      .setFooter("\u3000".repeat(10) + "|")
       .setTimestamp();
 
     channel.send({ content: "보스러쉬 계산기 입니다.", embeds: [embed] });
